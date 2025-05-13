e6ef438e35eaa7f2a240bc71adb3849c561c4ea9
Andrewmc123 <andrewmcclain123@gmail.com>
2025-05-13 00:08:50 -0400

    fronend auth

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -3,18 +3,14 @@ import { csrfFetch } from './csrf';
 const SET_USER = 'session/setUser';
 const REMOVE_USER = 'session/removeUser';
 
-const setUser = (user) => {
-  return {
-    type: SET_USER,
-    payload: user
-  };
-};
+const setUser = (user) => ({
+  type: SET_USER,
+  payload: user
+});
 
-const removeUser = () => {
-  return {
-    type: REMOVE_USER
-  };
-};
+const removeUser = () => ({
+  type: REMOVE_USER
+});
 
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
@@ -51,6 +47,14 @@ export const restoreUser = () => async (dispatch) => {
   const response = await csrfFetch('/api/session');
   const data = await response.json();
   dispatch(setUser(data.user));
+  return response;
+};
+
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE'
+  });
+  dispatch(removeUser());
   return response;
 };
 
