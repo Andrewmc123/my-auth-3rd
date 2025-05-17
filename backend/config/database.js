1c9e009ef95b5c6f0a89f5d92e31d69aadd32df7
Andrewmc123 <andrewmcclain123@gmail.com>
2025-05-17 00:27:33 -0400

    updated database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,19 +1,25 @@
+const config = require('./index');
+
 module.exports = {
   development: {
-    dialect: 'sqlite',
-    storage: './database.sqlite'
+    storage: config.dbFile,
+    dialect: "sqlite",
+    seederStorage: "sequelize",
+    logQueryParameters: true,
+    typeValidation: true
   },
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    seederStorage: 'sequelize',
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false
       }
     },
-    seederStorage: 'sequelize',
-    logQueryParameters: true,
-    typeValidation: true
+    define: {
+      schema: process.env.SCHEMA
+    }
   }
 };
