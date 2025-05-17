fa3b22f4e22b979187972e7afe97fbf69efa1b5f
Andrewmc123 <andrewmcclain123@gmail.com>
2025-05-16 21:16:07 -0400

    remove duplicate SQL migration

--- backend/db/migrations/20250517_add_first_name_to_users.sql
+++
@@ -1,5 +0,0 @@
--- Add firstName column to Users table
-ALTER TABLE "Users" ADD COLUMN "firstName" VARCHAR(50);
-
--- Add NOT NULL constraint if needed
--- ALTER TABLE "Users" ALTER COLUMN "firstName" SET NOT NULL;
