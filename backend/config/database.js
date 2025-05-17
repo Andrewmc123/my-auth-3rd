647f2833a00ccfb4998b0b6bdb88a5daf7bab56f
Andrewmc123 <andrewmcclain123@gmail.com>
2025-05-16 21:24:50 -0400

    update database

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,7 +6,6 @@ module.exports = {
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
-    schema: process.env.SCHEMA || 'my_auth',
     dialectOptions: {
       ssl: {
         require: true,
